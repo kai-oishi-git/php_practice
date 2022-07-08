4ae2d30c3dd0a1e173a94ee559a65b1fe011f201
kai-oishi-git <kai.oishi.git@gmail.com>
2022-07-08 16:41:13 +0900

    auto_increment指定

--- db/CreateTable.php
+++ db/CreateTable.php
@@ -8,13 +8,13 @@ function createEmployeeTable() {
     }
 
     $query = "CREATE TABLE employee (
-        id_employee int,
+        id_employee int AUTO_INCREMENT,
         nm_employee varchar(50),
         kn_employee varchar(50),
         mail varchar(200),
         password varchar(20),
         id_department int
-        )";
+        )AUTO_INCREMENT = 10000;";
 
     $stmt = $mysql->prepare($query);
     $stmt->execute();
